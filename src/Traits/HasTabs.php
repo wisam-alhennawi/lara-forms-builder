e8aaf95bf778b7edc5565a445afa33672706ddd7
Wisam Alhennawi <wisam.alhennawi@schwinge.com>
2023-03-21 17:14:21 +0100

    Init Active tab

--- src/Traits/HasTabs.php
+++ src/Traits/HasTabs.php
@@ -6,4 +6,18 @@ trait HasTabs
 {
     public $activeTab = '';
     public $hasTabs = true;
+
+    protected function initActiveTab($value)
+    {
+        // example of value: 'user-details' => 'InitUserDetailsTab'
+        $functionName = 'Init'.str_replace('-', '', ucwords($value, '-')).'Tab';
+        if (method_exists($this, $functionName)) {
+            $this->{$functionName}($value);
+        }
+    }
+
+    protected function updatedActiveTab($value)
+    {
+        $this->initActiveTab($value);
+    }
 }

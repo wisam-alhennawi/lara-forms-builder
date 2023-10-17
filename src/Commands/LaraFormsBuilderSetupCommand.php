79ea309a9fc985ed865f690939dcf31f971cfda1
Hussam Alhennawi <alhennawi.hussam@gmail.com>
2023-10-17 12:08:36 +0400

    improve command output style

--- src/Commands/LaraFormsBuilderSetupCommand.php
+++ src/Commands/LaraFormsBuilderSetupCommand.php
@@ -112,30 +112,30 @@ class LaraFormsBuilderSetupCommand extends Command
     protected function runInstallations(): void
     {
         $this->installJetstream();
+        $this->installPikadayAndMoment();
+        $this->includeConfirmationModal();
         $this->publishLaraFormsBuilderConfigFile();
         $this->publishLaraFormsBuilderCSSFile();
-        $this->includeConfirmationModal();
-        $this->installPikadayAndMoment();
         $this->npmActions();
     }
 
     protected function installJetstream(): void
     {
         if (! $this->option('stv') && ! $this->isJetstreamInstalled) {
-            if ($this->components->confirm('This Package Requires (laravel/jetstream:^3.0 with livewire/livewire:^2.0) Do You Want To Install them ?', true)) {
+            if ($this->components->confirm('This package qequires (laravel/jetstream:^3.0 with livewire/livewire:^2.0). Do you want to install them?', true)) {
                 try {
                     exec('composer require laravel/jetstream:^3.0');
                     exec('php artisan jetstream:install livewire');
                     exec('php artisan migrate');
 
                     $this->line('');
-                    $this->components->info('<options=bold,reverse;fg=green> Jetstream with livewire installed successfully & DB migrated </>');
+                    $this->components->info('(laravel/jetstream:^3.0 with livewire/livewire:^2.0) installed successfully.');
                 } catch (\Throwable $e) {
-                    $this->components->error('Jetstream with livewire installation ERROR');
+                    $this->components->error('(laravel/jetstream:^3.0 with livewire/livewire:^2.0) installation ERROR.');
                     exit;
                 }
             } else {
-                $this->components->warn('Installation Aborted Because Required Packages Not Installed');
+                $this->components->warn('Installation aborted because required packages not installed.');
                 exit;
             }
         }
@@ -145,7 +145,7 @@ class LaraFormsBuilderSetupCommand extends Command
     {
         if (! $this->isLaraFormsBuilderConfigFilePublished) {
             $this->call('vendor:publish', ['--tag' => 'lara-forms-builder-config', '--force' => true]);
-
+            $this->components->info('(lara-forms-builder-config) published successfully.');
             // add lara-forms-builder-config to the content[] in tailwind.config.js
             (new Filesystem)
                 ->replaceInFile(
@@ -153,8 +153,6 @@ class LaraFormsBuilderSetupCommand extends Command
                     "'./resources/views/**/*.blade.php',\n        './config/lara-forms-builder.php',",
                     base_path('tailwind.config.js')
                 );
-
-            $this->components->info('<options=bold,reverse;fg=green> (lara-forms-builder-config) published successfully & added to tailwind.config.js </>');
         }
     }
 
@@ -162,7 +160,7 @@ class LaraFormsBuilderSetupCommand extends Command
     {
         if (! $this->isLaraFormsBuilderCSSFilePublished) {
             $this->call('vendor:publish', ['--tag' => 'lara-forms-builder-assets', '--force' => true]);
-
+            $this->components->info('(lara-forms-builder-assets) published successfully.');
             // import lara-forms-builder-assets in app.css
             (new Filesystem)
                 ->prepend(
@@ -187,22 +185,19 @@ class LaraFormsBuilderSetupCommand extends Command
 
                     base_path('tailwind.config.js')
                 );
-
-            $this->components->info('<options=bold,reverse;fg=green> (lara-forms-builder-assets) published successfully & imported in /resource/css/app.css & colors added in tailwind.config.js </>');
         }
     }
 
     protected function includeConfirmationModal(): void
     {
         if (! $this->isConfirmationModalIncluded) {
-            if ($this->components->confirm('Do You Want To Include Modal in app.blade layout ?', true)) {
+            if ($this->components->confirm('Do you want to include confirmation modal in app.blade layout?', true)) {
                 (new Filesystem)
                     ->replaceInFile(
                         "@livewire('navigation-menu')",
                         "@livewire('navigation-menu') \n            @livewire('modals.confirmation')",
                         resource_path('views/layouts/app.blade.php')
                     );
-                $this->components->info('<options=bold,reverse;fg=green> Confirmation modal included successfully </>');
             }
         }
     }
@@ -210,7 +205,7 @@ class LaraFormsBuilderSetupCommand extends Command
     protected function installPikadayAndMoment(): void
     {
         if (! $this->isPikadayInstalled || ! $this->isMomentInstalled) {
-            if ($this->components->confirm('Do You Want To Install and config (pikaday & moment) npm packages, These packages are required to use date fields in lara-forms-builder ?', true)) {
+            if ($this->components->confirm('Do you want to install and config (pikaday & moment) npm packages, These packages are required to use date fields in lara-forms-builder?', true)) {
                 $data = json_decode(file_get_contents(base_path('package.json')), true);
                 if (! $this->isPikadayInstalled) {
                     $data['devDependencies']['pikaday'] = '^1.8.2';
@@ -220,6 +215,7 @@ class LaraFormsBuilderSetupCommand extends Command
                 }
 
                 file_put_contents(base_path('package.json'), json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE));
+                $this->components->info('(pikaday & moment) packages added to package.json successfully.');
 
                 // config pikaday in app.js
                 (new Filesystem)
@@ -234,8 +230,6 @@ class LaraFormsBuilderSetupCommand extends Command
                         resource_path('/css/app.css'),
                         '@import "pikaday/css/pikaday.css";'."\n"
                     );
-
-                $this->components->info('<options=bold,reverse;fg=green> (pikaday & moment) packages added to package.json successfully & imported in (app.js & app.css) </>');
             }
         }
     }
@@ -243,9 +237,9 @@ class LaraFormsBuilderSetupCommand extends Command
     protected function npmActions(): void
     {
         try {
-            $this->components->info('<options=bold,reverse;fg=green> Running npm install & npm run build </>');
             $this->info(exec('npm install'));
             $this->info(exec('npm run build'));
+            $this->components->info('npm install & npm run build executed successfully.');
         } catch (\Throwable $e) {
             $this->components->error('(npm install & npm run build) ERROR');
             exit;

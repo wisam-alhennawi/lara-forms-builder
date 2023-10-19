3a07c1f173662dca6c7727f07fcfff26f6f92bb7
Hussam Alhennawi <alhennawi.hussam@gmail.com>
2023-10-19 15:47:49 +0400

    fix php stan error

--- src/Traits/WorkingWithStringsInFilesTrait.php
+++ src/Traits/WorkingWithStringsInFilesTrait.php
@@ -4,13 +4,14 @@ namespace WisamAlhennawi\LaraFormsBuilder\Traits;
 
 trait WorkingWithStringsInFilesTrait
 {
-    public function insertInFile($search, $insert, $path, $after = true): ?bool
+    public function insertInFile($search, $insert, $path, $after = true): bool
     {
         $errorMsg = "Couldn't add '".trim($insert)."' to '$path'. Please add it manually.";
 
         // check if the file not exists
         if (! file_exists($path)) {
-            return $this->components->warn($errorMsg);
+            $this->components->warn($errorMsg);
+            return false;
         }
         // check if the file already has the $string
         if (str_contains($fileContentsAsString = file_get_contents($path), $insert)) {
@@ -18,7 +19,8 @@ trait WorkingWithStringsInFilesTrait
         }
         // check if the $keyword we want to add $string after it not in the file
         if (! str_contains($fileContentsAsString, $search)) {
-            return $this->components->warn($errorMsg);
+            $this->components->warn($errorMsg);
+            return false;
         }
 
         if ($after) {
@@ -27,16 +29,23 @@ trait WorkingWithStringsInFilesTrait
             file_put_contents($path, substr_replace($fileContentsAsString, $insert.PHP_EOL, strpos($fileContentsAsString, $search), 0));
         }
 
-        return str_contains(file_get_contents($path), $insert) ?? $this->components->warn($errorMsg);
+        // check if the $insert string inserted or not
+        if (str_contains(file_get_contents($path), $insert)) {
+            return true;
+        } else {
+            $this->components->warn($errorMsg);
+            return false;
+        }
     }
 
-    public function replaceInFile($search, $replace, $path): ?bool
+    public function replaceInFile($search, $replace, $path): bool
     {
         $errorMsg = "Couldn't replace \"".trim($search).'" with "'.trim($replace)."\" in $path Please replace it manually.";
 
         // check if the file not exists
         if (! file_exists($path)) {
-            return $this->components->warn($errorMsg);
+            $this->components->warn($errorMsg);
+            return false;
         }
         // check if the file already has the string $replace
         if (str_contains($fileContentsAsString = file_get_contents($path), $replace)) {
@@ -44,11 +53,18 @@ trait WorkingWithStringsInFilesTrait
         }
         // check if the string $search we want to replace it with $replace not in the file
         if (! str_contains($fileContentsAsString, $search)) {
-            return $this->components->warn($errorMsg);
+            $this->components->warn($errorMsg);
+            return false;
         }
 
         file_put_contents($path, str_replace($search, $replace, $fileContentsAsString));
 
-        return str_contains(file_get_contents($path), $replace) ?? $this->components->warn($errorMsg);
+        // check if the $replace string replaced or not
+        if (str_contains(file_get_contents($path), $replace)) {
+            return true;
+        } else {
+            $this->components->warn($errorMsg);
+            return false;
+        }
     }
 }

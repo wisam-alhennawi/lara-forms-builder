8151de0f7a547d16b18f04ccf0e7dfff3d1b4a32
Nico Andriano <nico.andriano@schwinge.com>
2023-10-24 11:57:19 +0200

    Add custom css class to cards layout

--- resources/views/form-components.blade.php
+++ resources/views/form-components.blade.php
@@ -65,6 +65,7 @@
             'readOnly' => isset($field['readOnly']) ? $field['readOnly'] : false,
             'fieldWrapperClass' => isset($field['field_wrapper_class']) ? $field['field_wrapper_class'] : $defaultFieldWrapperClass,
             'cardFieldErrorWrapperClass' => isset($field['card_field_error_wrapper_class']) ? $field['card_field_error_wrapper_class'] : $defaultCardFieldErrorWrapperClasses,
+            'icon' => isset($field['icon']) ? $field['icon'] : null,
         ])
         @break
     @case('checkbox-group')

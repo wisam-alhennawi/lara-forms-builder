f11b997639440818389fbeb51c03423ab2cdbed3
Hussam Alhennawi <alhennawi.hussam@gmail.com>
2023-10-26 19:02:12 +0400

    add group option to the select component

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -7,28 +7,58 @@
     </label>
     @php
         $fieldValue = $model->$key->value ?? $model->$key;
-        $optionKey = array_search($fieldValue, array_column($selectOptions, 'value'));
-        if (!is_numeric($optionKey) && $optionKey == false) {
-            $optionKey = array_search($this->$key, array_column($selectOptions, 'value'));
+        if ($isGrouped) {
+            $optionKeyGroupLabel = '';
+            foreach ($selectOptions as $groupLabel => $options) {
+                $optionKey = array_search($fieldValue, array_column($options, 'value'));
+                if (!is_numeric($optionKey) && $optionKey == false) {
+                    $optionKey = array_search($this->$key, array_column($options, 'value'));
+                }
+                if (is_numeric($optionKey)) {
+                    $optionKeyGroupLabel = $groupLabel;
+                    break;
+                }
+            }
+        } else {
+            $optionKey = array_search($fieldValue, array_column($selectOptions, 'value'));
+            if (!is_numeric($optionKey) && $optionKey == false) {
+                $optionKey = array_search($this->$key, array_column($selectOptions, 'value'));
+            }
         }
     @endphp
     @if ((isset($mode) && ($mode == 'view' || $mode == 'confirm')) || isset($readOnly) && $readOnly)
         <div class="lfb-input-wrapper">
-            <input type="text" name="{{ $key }}" id="{{ $key }}" value="@if(is_numeric($optionKey) && array_key_exists($optionKey, $selectOptions)){{ $selectOptions[$optionKey]['label'] }}@else - @endif" class="lfb-input lfb-disabled" readonly disabled>
+            <input type="text" name="{{ $key }}" id="{{ $key }}"
+                   value=@if($isGrouped && $optionKeyGroupLabel)
+                              {{ $selectOptions[$optionKeyGroupLabel][$optionKey]['label'] }}
+                          @elseif(is_numeric($optionKey) && array_key_exists($optionKey, $selectOptions))
+                              {{ $selectOptions[$optionKey]['label'] }}
+                          @else - @endif
+                   class="lfb-input lfb-disabled" readonly disabled>
         </div>
     @else
         <div class="lfb-input-wrapper">
             <select id="{{ $key }}" name="{{ $key }}" class="lfb-input"
-                wire:model="{{ $key }}">
+                    wire:model="{{ $key }}">
                 <option value>{{ __('Please select...') }}</option>
-                @foreach($selectOptions as $option)
-                <option value="{{ $option['value'] }}">{{ $option['label'] }}</option>
-                @endforeach
+                @if($isGrouped)
+                    @foreach($selectOptions as $groupLabel => $options)
+                        <optgroup label="{{ $groupLabel }}">
+                            @foreach($options as $option)
+                                <option value="{{ $option['value'] }}">{{ $option['label'] }}</option>
+                            @endforeach
+                        </optgroup>
+                    @endforeach
+                @else
+                    @foreach($selectOptions as $option)
+                        <option value="{{ $option['value'] }}">{{ $option['label'] }}</option>
+                    @endforeach
+                @endif
             </select>
         </div>
         @error($key) <span class="lfb-alert lfb-alert-error">{{ $message }}</span> @enderror
         @if(isset($helpText))
-        <p class="lfb-help-text">{{ $helpText }}</p>
+            <p class="lfb-help-text">{{ $helpText }}</p>
         @endif
     @endif
 </div>

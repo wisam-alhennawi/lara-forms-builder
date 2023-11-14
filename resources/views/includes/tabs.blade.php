5f961ea503d5af440b20e77bef39ded882c0099d
Nico Andriano <nico.andriano@schwinge.com>
2023-11-14 15:01:45 +0100

    Add custom icon to tabs  warning message.

--- resources/views/includes/tabs.blade.php
+++ resources/views/includes/tabs.blade.php
@@ -25,7 +25,13 @@
                     <div class="lfb-tab-error-wrapper" role="alert">
                         <div class="lfb-tab-error-container">
                             <div class="lfb-tab-error-icon flex justify-center items-center">
-                                <x-heroicon-o-exclamation-circle  />
+                                @if(isset($tabWarningIcon))  
+                                    {!! $tabWarningIcon !!}
+                                @else
+                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z"></path>
+                                 </svg>
+                                @endif    
                             </div>
                             <div class="lfb-tab-error-message">
                                 <p>{{ $message }}</p>

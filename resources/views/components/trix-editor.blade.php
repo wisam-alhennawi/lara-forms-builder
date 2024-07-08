e79fbf3979940ca43eb0f81f57829c062d6676a8
Hussam Alhennawi <alhennawi.hussam@gmail.com>
2024-07-08 20:06:27 +0400

    Improve trix-editor to be available to use more than one at the same form.

--- resources/views/components/trix-editor.blade.php
+++ resources/views/components/trix-editor.blade.php
@@ -2,9 +2,12 @@
     @include('lara-forms-builder::includes.field-label')
     @if (isset($mode) && ($mode == 'view' || $mode == 'confirm'))
         <div class="lfb-trix-editor-wrapper lfb-trix-editor-readonly">
-            <input value="{{ $formProperties[$key] }}" id="trix-editor-input-content" type="hidden"/>
+            <input id="trix-editor-input-content-{{ $key }}"
+                   value="{{ $formProperties[$key] }}"
+                   type="hidden" />
+
             <div wire:ignore>
-                <trix-editor input="trix-editor-input-content" class="trix-editor"></trix-editor>
+                <trix-editor input="trix-editor-input-content-{{ $key }}" class="trix-editor"></trix-editor>
             </div>
 
             <script>
@@ -14,15 +17,19 @@
         </div>
     @else
         <div class="lfb-trix-editor-wrapper">
-            <input id="trix-editor-input-content" type="hidden" value="{{ $formProperties[$key] }}"/>
+            <input id="trix-editor-input-content-{{ $key }}"
+                   value="{{ $formProperties[$key] }}"
+                   type="hidden" />
+
             <div wire:ignore>
-                <trix-editor input="trix-editor-input-content" class="trix-editor"></trix-editor>
+                <trix-editor input="trix-editor-input-content-{{ $key }}" class="trix-editor"></trix-editor>
             </div>
+
             <script>
-                let trixEditor = document.getElementById("trix-editor-input-content")
+                let trixEditor_{{ $key }} = document.getElementById("trix-editor-input-content-{{ $key }}")
 
                 document.addEventListener("trix-change", function(event) {
-                    @this.set("formProperties.{{ $key }}", trixEditor.getAttribute('value'));
+                    @this.set("formProperties.{{ $key }}", trixEditor_{{ $key }}.getAttribute('value'));
                 })
             </script>
             @include('lara-forms-builder::includes.field-error-message')

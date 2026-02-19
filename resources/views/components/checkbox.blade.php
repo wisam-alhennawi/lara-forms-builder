f01e8bb873898d53b51978adb77280f092bd3a7b
Nico Andriano <nico.andriano@schwinge.com>
2026-02-19 15:24:53 +0100

    Refactored the checkbox and yes-no-toggle components to be used within the accordion group.

--- resources/views/components/checkbox.blade.php
+++ resources/views/components/checkbox.blade.php
@@ -1,27 +1,49 @@
-<div class="{{ $fieldWrapperClass }} @error('formProperties.' .  $key){{ $fieldErrorWrapperClass }}@enderror">
-    <fieldset class="lfb-fieldset">
-        <div class="lfb-fieldset-container">
-            <div class="lfb-fieldset-item">
-                <input wire:key="form-checkbox-component-{{ md5($key) }}"
-                    wire:model.live="formProperties.{{ $key }}"
-                    id="formProperties-{{ $key }}"
-                    name="formProperties.{{ $key }}"
-                    type="checkbox"
-                    @class([
-                        'lfb-checkbox',
-                        'lfb-readonly' => (isset($mode) && ($mode == 'view' || $mode == 'confirm')) || $readOnly,
-                    ])
-                    @readonly((isset($mode) && ($mode == 'view' || $mode == 'confirm')) || $readOnly)
-                    @disabled((isset($mode) && ($mode == 'view' || $mode == 'confirm')) || $readOnly)
-                >
-                <label class="lfb-label lfb-label-spacing print:text-xs" for="formProperties-{{ $key }}">{!! $label !!}
-                @if ((!isset($mode) || (isset($mode) and $mode != 'view')) and isset($rules) and array_key_exists('formProperties.' .  $key, $rules) && str_contains($rules['formProperties.' .  $key], 'required'))
-                    <sup>*</sup>
-                @endif
-                </label>
+@php
+    $variant = $variant ?? 'default';
+    $alpineModel = $alpineModel ?? null;
+@endphp
+
+@if ($variant === 'accordion')
+    <input wire:key="form-checkbox-component-{{ md5($key) }}"
+        wire:model.live="formProperties.{{ $key }}"
+        @if($alpineModel) x-model="{{ $alpineModel }}" @endif
+        id="formProperties-{{ $key }}"
+        name="formProperties.{{ $key }}"
+        type="checkbox"
+        @class([
+            'lfb-checkbox',
+            'lfb-readonly' => (isset($mode) && ($mode == 'view' || $mode == 'confirm')) || $readOnly,
+        ])
+        @readonly((isset($mode) && ($mode == 'view' || $mode == 'confirm')) || $readOnly)
+        @disabled((isset($mode) && ($mode == 'view' || $mode == 'confirm')) || $readOnly)
+    >
+@else
+    <div class="{{ $fieldWrapperClass }} @error('formProperties.' .  $key){{ $fieldErrorWrapperClass }}@enderror">
+        <fieldset class="lfb-fieldset">
+            <div class="lfb-fieldset-container">
+                <div class="lfb-fieldset-item">
+                    <input wire:key="form-checkbox-component-{{ md5($key) }}"
+                        wire:model.live="formProperties.{{ $key }}"
+                        @if($alpineModel) x-model="{{ $alpineModel }}" @endif
+                        id="formProperties-{{ $key }}"
+                        name="formProperties.{{ $key }}"
+                        type="checkbox"
+                        @class([
+                            'lfb-checkbox',
+                            'lfb-readonly' => (isset($mode) && ($mode == 'view' || $mode == 'confirm')) || $readOnly,
+                        ])
+                        @readonly((isset($mode) && ($mode == 'view' || $mode == 'confirm')) || $readOnly)
+                        @disabled((isset($mode) && ($mode == 'view' || $mode == 'confirm')) || $readOnly)
+                    >
+                    <label class="lfb-label lfb-label-spacing print:text-xs" for="formProperties-{{ $key }}">{!! $label !!}
+                    @if ((!isset($mode) || (isset($mode) and $mode != 'view')) and isset($rules) and array_key_exists('formProperties.' .  $key, $rules) && str_contains($rules['formProperties.' .  $key], 'required'))
+                        <sup>*</sup>
+                    @endif
+                    </label>
+                </div>
             </div>
-        </div>
-        @include('lara-forms-builder::includes.field-error-message')
-        @include('lara-forms-builder::includes.field-help-text')
-    </fieldset>
-</div>
+            @include('lara-forms-builder::includes.field-error-message')
+            @include('lara-forms-builder::includes.field-help-text')
+        </fieldset>
+    </div>
+@endif
--- resources/views/components/yes-no-toggle-switch.blade.php
+++ resources/views/components/yes-no-toggle-switch.blade.php
@@ -1,4 +1,6 @@
 @php
+    $variant = $variant ?? 'default';
+    $alpineModel = $alpineModel ?? null;
     $values = array_keys($toggleOptions);
     $noValue = $values[0] ?? 0;
     $yesValue = $values[1] ?? 1;
@@ -15,20 +17,16 @@
     $isDisabled = (isset($mode) && ($mode == 'view' || $mode == 'confirm')) || $readOnly;
 @endphp
 
-<div class="{{ $fieldWrapperClass }} @error('formProperties.' . $key){{ $fieldErrorWrapperClass }}@enderror">
-    @include('lara-forms-builder::includes.field-label')
-    
+@if ($variant === 'accordion')
     <div class="lfb-yes-no-toggle-container" 
          role="group" 
-         aria-labelledby="label-{{ $key }}"
-         aria-describedby="@if($helpText)help-{{ $key }}@endif">
-        
+         aria-labelledby="label-{{ $key }}">
         <div class="lfb-yes-no-toggle-wrapper @if($isDisabled) lfb-yes-no-toggle-disabled @endif">
-            
             <!-- Button Yes -->
             <button type="button"
                     class="lfb-yes-no-toggle-label lfb-yes-no-toggle-label-on {{ (string) $currentValue === (string) $yesValue ? 'lfb-yes-no-toggle-active' : '' }}"
                     wire:click="$set('formProperties.{{ $key }}', '{{ $yesValue }}')"
+                    @if($alpineModel) x-on:click="{{ $alpineModel }} = true" @endif
                     @disabled($isDisabled)
                     aria-pressed="{{ (string) $currentValue === (string) $yesValue ? 'true' : 'false' }}">
                 {{ $yesLabel }}
@@ -45,14 +43,52 @@
             <button type="button"
                     class="lfb-yes-no-toggle-label lfb-yes-no-toggle-label-off {{ (string) $currentValue === (string) $noValue ? 'lfb-yes-no-toggle-active' : '' }}"
                     wire:click="$set('formProperties.{{ $key }}', '{{ $noValue }}')"
+                    @if($alpineModel) x-on:click="{{ $alpineModel }} = false" @endif
                     @disabled($isDisabled)
                     aria-pressed="{{ (string) $currentValue === (string) $noValue ? 'true' : 'false' }}">
                 {{ $noLabel }}
             </button>
-            
         </div>
     </div>
-    
-    @include('lara-forms-builder::includes.field-error-message')
-    @include('lara-forms-builder::includes.field-help-text')
-</div>
+@else
+    <div class="{{ $fieldWrapperClass }} @error('formProperties.' . $key){{ $fieldErrorWrapperClass }}@enderror">  
+        @include('lara-forms-builder::includes.field-label')
+        <div class="lfb-yes-no-toggle-container" 
+             role="group" 
+             aria-labelledby="label-{{ $key }}"
+             aria-describedby="@if($helpText)help-{{ $key }}@endif">
+            
+            <div class="lfb-yes-no-toggle-wrapper @if($isDisabled) lfb-yes-no-toggle-disabled @endif">
+                
+                <!-- Button Yes -->
+                <button type="button"
+                        class="lfb-yes-no-toggle-label lfb-yes-no-toggle-label-on {{ (string) $currentValue === (string) $yesValue ? 'lfb-yes-no-toggle-active' : '' }}"
+                        wire:click="$set('formProperties.{{ $key }}', '{{ $yesValue }}')"
+                        @disabled($isDisabled)
+                        aria-pressed="{{ (string) $currentValue === (string) $yesValue ? 'true' : 'false' }}">
+                    {{ $yesLabel }}
+                </button>
+                
+                <!-- Hidden input to maintain form state -->
+                <input type="hidden" 
+                       wire:model.live="formProperties.{{ $key }}"
+                       id="formProperties-{{ $key }}"
+                       name="formProperties.{{ $key }}"
+                       value="{{ $currentValue }}">
+                
+                <!-- Button No -->
+                <button type="button"
+                        class="lfb-yes-no-toggle-label lfb-yes-no-toggle-label-off {{ (string) $currentValue === (string) $noValue ? 'lfb-yes-no-toggle-active' : '' }}"
+                        wire:click="$set('formProperties.{{ $key }}', '{{ $noValue }}')"
+                        @disabled($isDisabled)
+                        aria-pressed="{{ (string) $currentValue === (string) $noValue ? 'true' : 'false' }}">
+                    {{ $noLabel }}
+                </button>
+                
+            </div>
+        </div>
+        
+        @include('lara-forms-builder::includes.field-error-message')
+        @include('lara-forms-builder::includes.field-help-text')
+    </div>
+@endif
